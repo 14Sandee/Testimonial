cefac3cc6c41b0a3d2d3470e086749a97c09b1d2
14Sandee <55476652+14Sandee@users.noreply.github.com>
2021-09-03 19:23:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, CSSProperties } from 'react';
 import './App.css';
+import { Testimonial } from './Testimonial';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 
 function App() {
+
+  const arrowStyles = {
+            position: 'absolute',
+            zIndex: 2,
+            top: 'calc(50% - 15px)',
+            width: 30,
+            height: 30,
+            cursor: 'pointer',
+        };
+
+  const [testimonial, setTestimonial] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await fetch("https://testimonialapi.toolcarton.com/api").then(response => response.json())
+      console.log(request);
+      setTestimonial(request);
+      return request;
+    }
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Testimonial</h1>
+        <Carousel
+          autoPlay
+          infiniteLoop
+          showStatus={false}
+          showThumbs={false}
+          showIndicators={false}
+          interval={5000}
+          statusFormatter={(current, total) => `Current slide: ${current} / Total: ${total}`}
+            renderArrowPrev={(onClickHandler, hasPrev, label) =>
+                hasPrev && (
+                    <button className="slider__btn" type="button" onClick={onClickHandler} title={label} style={{...arrowStyles, left: 0 }}>
+                        <i className="ri-arrow-left-s-line"></i>
+                    </button>
+                )
+            }
+            renderArrowNext={(onClickHandler, hasNext, label) =>
+                hasNext && (
+                    <button className="slider__btn" type="button" onClick={onClickHandler} title={label} style={{...arrowStyles, right: 0 }}>
+                        <i className="ri-arrow-right-s-line"></i>
+                    </button>
+                )
+            }
         >
-          Learn React
-        </a>
+        {testimonial?.map(({ id, audio, avatar, designation, location, lorem, message, name, rating }) => (
+            <Testimonial 
+              key={id}
+              audio={audio}
+              avatar={avatar}
+              designation={designation}
+              location={location}
+              lorem={lorem}
+              message={message}
+              name={name}
+              rating={rating}
+            />
+        ))}
+        </Carousel>
       </header>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/Testimonial.js
@@ -0,0 +1,28 @@
+import React, {CSSProperties} from 'react'
+import './Testimonial.css'
+import 'remixicon/fonts/remixicon.css'
+
+export const Testimonial = ({ audio, avatar, designation, location, lorem, message, name, rating }) => {
+    return (
+        <div className="testimonial">
+            <div className="testimonial__image">
+                <img src={avatar} alt="" />
+            </div>
+            <div className="testimonial__content">
+                <p className="testimonial__message"><i className="ri-double-quotes-l" />{message}</p>
+                <div className="testimonial__footer">
+                    <div className="testimonial__footer__left">
+                        <h4 className="testimonial__heading">{name}</h4>
+                        <div className="testimonial__subHeading">
+                            <p className="testimonial__para">{designation}</p>
+                            <p className="testimonial__para">, {location}</p>
+                        </div> 
+                    </div>
+                    <div className="testimonial__footer__right">
+                        <p className="testimonial__para">{`Rating: ${rating}`}<i className="ri-star-fill"/></p>
+                    </div>   
+                </div>
+            </div>
+        </div>  
+    )
+}
