10273525c7b83f0c5e5253faa9f764256d6d7a9b
14Sandee <55476652+14Sandee@users.noreply.github.com>
2021-09-03 19:30:55 +0530

    CSSProperties removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, CSSProperties } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Testimonial } from './Testimonial';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
